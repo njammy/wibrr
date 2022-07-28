5e086126628e3df7594aa39413c40f56573482e4
njammy <njammypatchakwejovani@gmail.com>
2022-07-28 03:52:40 +0100

    feat: frontend ui struct and mock complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,53 @@
 import './App.css';
+import { useState } from 'react'
+import Data from "./mock-data.json"
 
 function App() {
+  const [query, setQuery] = useState("")
   return (
     <div className="App">
       <div className="wibrr-space">
-
+        <input placeholder="Enter Product Name" onChange={event => setQuery(event.target.value)}/>
+        <div className='results'>
+          <div className='article-result'>
+            {
+              Data.filter(product => {
+                if (query === '') {
+                  return [];
+                } else if (product.name.toLowerCase().includes(query.toLowerCase())) {
+                  return product;
+                }
+              }).map((product, index) => (
+                <div className="box box-best" key={index}>
+                  <p>
+                    Name: {product.name} <br/>
+                    Price: {product.price}  <br/>
+                    OldPrice: {product.oldprice}
+                  </p>
+                </div>
+              ))
+            }
+          </div>
+          <div className='article-result'>
+            {
+              Data.filter(product => {
+                if (query === '') {
+                  return [];
+                } else if (product.name.toLowerCase().includes(query.toLowerCase())) {
+                  return product;
+                }
+              }).map((product, index) => (
+                <div className="box" key={index}>
+                  <p>
+                    Name: {product.name} <br/>
+                    Price: {product.price}  <br/>
+                    OldPrice: {product.oldprice}
+                  </p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
       </div>
     </div>
   );
